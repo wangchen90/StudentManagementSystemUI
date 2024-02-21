10515aa39ba6df7fe3c7b41cfada014905e3d11b
wangchen90 <wangchensherpa1@gmail.com>
2024-02-21 15:12:36 +0530

    Init

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserloginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
